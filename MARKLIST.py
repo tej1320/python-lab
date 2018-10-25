f06a97c65a5e7ff050c96fdf2d12f91faa9f8d2c
Tejas Karwa <44194557+tej1320@users.noreply.github.com>
2018-10-25 10:36:09 -0700

    Add files via upload

---
+++ MARKLIST.py
@@ -0,0 +1,17 @@
+name1=input("enter 1st student name")
+name2=input("enter 2st student name")
+name3=input("enter 3st student name")
+english1=input("marks of english for student1")
+english2=input("marks of english for student2")
+english3=input("marks of english for student3")
+science1=input("marks for scince for student1")
+science2=input("marks for scince for student2")
+science3=input("marks for scince for student3")
+maths1=input("marks of maths for student1")
+maths2=input("marks of maths for student2")
+maths3=input("marks of maths for student3")
+student1=[name1,'english',english1,'science',science1,'maths',maths1]
+student2=[name2,'english',english2,'science',science2,'maths',maths2]
+student3=[name3,'english',english3,'science',science3,'maths',maths3]
+student=[student1,student2,student3]
+print(student)
